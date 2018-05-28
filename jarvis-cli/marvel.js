9e801dae29269560c8c4f8cbd1256913b5d57e11
Sean Lattaker <seanlattaker@Seans-MacBook-Air.local>
2018-05-28 16:48:52 -0400

    5charactersadded

--- jarvis-cli/marvel.js
+++ jarvis-cli/marvel.js
@@ -11,7 +11,7 @@ var api = require('marvel-api')
 
 }
 
-  getFirst100() {
+ /* getFirst100() {
   this.marvel.characters.findAll(100)
    .then(function(res) {
    for (var index = 0; index < res.data.length; index++) {
@@ -21,7 +21,82 @@ var api = require('marvel-api')
    .fail(console.error)
    .done();
 
-   }
+   
+}
+
+*/ 
+ 
+ getSpiderman(){
+ this.marvel.characters.findByName('spider-man')
+  .then(console.log)
+  .fail(console.error)
+  .done();
+}
+getThor(){
+
+ this.marvel.characters.findByName('thor')
+  .then(console.log)
+  .fail(console.error)
+  .done();
+
+}
+
+getFalcon(){
+
+ this.marvel.characters.findByName('falcon')
+  .then(console.log)
+  .fail(console.error)
+  .done();
+
+}
+
+
+getHulk(){
+
+ this.marvel.characters.findByName('hulk')
+  .then(console.log)
+  .fail(console.error)
+  .done();
+
+} 
+
+getQuicksilver(){
+
+ this.marvel.characters.findByName('quicksilver')
+  .then(console.log)
+  .fail(console.error)
+  .done();
+
+}
+
+ 
+getHawkeye(){
+
+ this.marvel.characters.findByName('hawkeye')
+  .then(console.log)
+  .fail(console.error)
+  .done();
+
+} 
+
+getVision(){
+
+ this.marvel.characters.findByName('vision')
+  .then(console.log)
+  .fail(console.error)
+  .done();
+
+} 
+
+getWasp(){
+
+ this.marvel.characters.findByName('wasp')
+  .then(console.log)
+  .fail(console.error)
+  .done();
+
+}
+
 }  
 
 module.exports = Marvel                    
