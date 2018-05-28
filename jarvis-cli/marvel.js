210deb1e0a60f983970c1b67ffa3072da0cd1254
Sean Lattaker <seanlattaker@Seans-MacBook-Air.local>
2018-05-28 15:39:33 -0400

    first100working

--- jarvis-cli/marvel.js
+++ jarvis-cli/marvel.js
@@ -1,20 +1,18 @@
 var api = require('marvel-api') 
 
-
-
- var marvel = api.createClient({
-   publicKey : 'd4d99791cfab71966cc8545ad4fbc6d2',
-   privateKey : 'ccfe3ec34335993e9d48dcc47901e80ee7362536'
-   });
-
   class Marvel{
-    constructor(){
+    constructor(config) {
+ this.marvel = api.createClient({
+  publicKey : config.publicKey,
+  privateKey : config.privateKey
+  });
+
 
 
 }
 
   getFirst100() {
-  marvel.characters.findAll(100)
+  this.marvel.characters.findAll(100)
    .then(function(res) {
    for (var index = 0; index < res.data.length; index++) {
    console.log((index + 1) + ' ' + res.data[index].name)
