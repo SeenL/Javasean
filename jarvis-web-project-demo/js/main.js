3ec8535bc5686ac6aa65e00f37f9043a2b6e7338
root <root@slattaker.localdomain>
2018-06-03 09:57:08 -0400

    good commit

--- jarvis-web-project-demo/js/main.js
+++ jarvis-web-project-demo/js/main.js
@@ -11,10 +11,20 @@ var card = document.createElement('div')
  card.className = 'w3-card-4'
  var img = document.createElement('img')
  if(arr[x].name == 'Hulk'){
-
-
   img.src = '/assets/littlehulk.png'
  }
+   else  if(arr[x].name == 'Captain America'){
+  img.src = '/assets/captainamerica.png'
+ }
+    else  if(arr[x].name == 'Spider-Man'){
+  img.src = '/assets/spiderman.png'
+ }
+   else  if(arr[x].name == 'Vision'){
+  img.src = '/assets/vision.png'
+ }
+   else  if(arr[x].name == 'Thor'){
+  img.src = '/assets/thorpic.png'
+ }  
  card.append(img)
  var cardContainer = document.createElement('div')
  cardContainer.className = 'w3-container w3-center'
