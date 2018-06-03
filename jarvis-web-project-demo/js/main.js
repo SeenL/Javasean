43274616c632011b9523e0786309c3591cddab9e
Sean Lattaker <slatta1@att.net>
2018-06-03 09:01:32 -0400

    last one

--- jarvis-web-project-demo/js/main.js
+++ jarvis-web-project-demo/js/main.js
@@ -1,16 +1,44 @@
-// we need a button to click
+//// we need a button to click
 // we need a function to go and hit my jarvis api - using XMLHttpRequest
 //<button onclick="myFunction()">Click me</button>
- 
 
+
+ 
+ var createCard = function(arr) {
+ for (var x=0; x<arr.length; x++){
+var card = document.createElement('div')
+ card.style.width = '25%'
+ card.className = 'w3-card-4'
+ var img = document.createElement('img')
+ if(arr[x].name == 'Hulk'){
+
+
+  img.src = '/assets/littlehulk.png'
+ }
+ card.append(img)
+ var cardContainer = document.createElement('div')
+ cardContainer.className = 'w3-container w3-center'
+ card.append(cardContainer) 
+ var note = document.createElement('p')
+ note.innerHTML = arr[x].description
+ cardContainer.append(note)
+
+
+ content.append(card)
+
+
+  }
+
+ }
  var getAvengers = function() {
   var req = new XMLHttpRequest()
   req.onreadystatechange = function() {
    if(this.readyState == 4){
-     console.log(JSON.parse(this.responseText))
+     createCard(JSON.parse(this.responseText)) 
+      }
 
      }
-   }
+   
      req.open('Get', 'http://localhost:4000')
      req.send()
  }
@@ -23,22 +51,11 @@
  button.onclick = getAvengers 
  button.innerHTML = 'Get Avengers'
 
-// Add a w3 CSS card with Image
-var card = document.createElement('div')
-card.className = 'w3-card-4'
-var img = document.createElement('img')
-img.src = '/assets/littlehulk.png'
-card.append(img) 
-var cardContainer = document.createElement('div')
-cardContainer.className = 'w3-container w3-center'
-
- 
 
 
  var h1 = document.createElement('h2')
  h1.innerHTML = 'Avengers Assemble' 
  content.appendChild(h1)
- content.append(card)
  content.append(button)
  
  
